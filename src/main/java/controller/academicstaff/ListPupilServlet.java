0a2c74f81f0ab5d839471874202ae1a5c059891c
Hoàng Anh Quân <hoangquan15012004@gmail.com>
2024-05-23 14:46:25 +0700

    add list of pupil ( acacdemic staff)
    function list of pupil in academicstaff screen

---
+++ src/main/java/controller/academicstaff/ListPupilServlet.java
@@ -0,0 +1,38 @@
+package controller.academicstaff;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import models.pupil.Pupil;
+import models.pupil.PupilDAO;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@WebServlet(name = "academicstaff/ListPupilServlet", value = "/academicstaff/listpupil")
+public class ListPupilServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PupilDAO pupildao = new PupilDAO();
+
+        List<Pupil> listPupil = new ArrayList<>();
+        /////  Create list of all pupil as a default list
+        listPupil = pupildao.getAllPupils();
+        /////  Get request from search function
+        String searchInformation = request.getParameter("information");
+        if(searchInformation !=null){
+            listPupil = pupildao.getListOfSearchPupilByNameOrId(searchInformation);
+        }
+        request.setAttribute("listPupil",listPupil);
+        request.getRequestDispatcher("staff_listpupil.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
